956efaea908fa89b46b663cd517f17304b3c4133
Sam Bertken <sam.bertken@wework.com>
2018-02-14 14:39:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,6 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == log10
-    puts "Fizzbuzz"
-    
-    elsif int % 5 == 0 
-    puts "Buzz"
-    
-    elsif int % 3 == 0 
-    puts "Fizz"
-  end
-end
+
 
 fizzbuzz(3)
 
